7160d28a54e741cb0a17fe86eeb4dcd869f7ad73
28JayG <jaygandhi.32@outlook.com>
2019-01-10 21:59:57 +0530

    Sends streams of SlideUpdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:material_page_reveal/model/page_indicator_view_model.dart';
 import 'package:material_page_reveal/model/page_view_model.dart';
 import 'package:material_page_reveal/page_indicator.dart';
 import 'package:material_page_reveal/pages.dart';
-//import 'package:material_page_reveal/widget/page_dragger.dart';
+import 'package:material_page_reveal/widget/page_dragger.dart';
 import 'package:material_page_reveal/widget/page_reveal.dart';
 
 main() {
@@ -26,6 +28,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+//TODO:(8.2) Bring in the StreamController to listen to the SlideUpdate
+//  StreamController<SlideUpdate> streamController;
+//
+//  int activeIndex = 0;
+//  SlideDirection slideDirection = SlideDirection.none;
+//  double slidePercent = 0.0;
+//  int nextIndex = 0;
+//
+//  _HomePageState() {
+//    streamController = StreamController<SlideUpdate>();
+//
+//    streamController.stream.listen((SlideUpdate event) {
+//      setState(() {
+//        if (event.dragUpdateType == DragUpdateType.dragging) {
+//        slideDirection = event.direction;
+//          slidePercent = event.percent;
+////          if(slideDirection == SlideDirection.leftToRight){
+////            nextIndex = activeIndex -1;
+////          }else if(slideDirection == SlideDirection.rightToLeft){
+////            nextIndex = activeIndex +1;
+////          }else{
+////           nextIndex = activeIndex;
+////          }
+//
+//          nextIndex = slideDirection == SlideDirection.leftToRight
+//              ? activeIndex - 1
+//              : activeIndex + 1;
+//
+//          nextIndex.clamp(0.0, viewModel.length - 1);
+//        } else if (event.dragUpdateType == DragUpdateType.doneDragging) {
+//          if (slidePercent > 0.5) {
+//            activeIndex = slideDirection == SlideDirection.leftToRight
+//                ? activeIndex - 1
+//                : activeIndex + 1;
+//          }
+//
+//          slideDirection = SlideDirection.none;
+//          slidePercent = 0.0;
+//        }
+//      });
+//    });
+//  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,11 +78,17 @@ class _HomePageState extends State<HomePage> {
         children: <Widget>[
           Page(
             pageViewModel: viewModel[0],
+//            pageViewModel: viewModel[activeIndex],
+            percentVisible: 1.0,
           ),
           PageReveal(
             revealPercent: 1.0,
+//            revealPercent: slidePercent,
             child: Page(
               pageViewModel: viewModel[1],
+              percentVisible: 1.0,
+//              pageViewModel: viewModel[nextIndex],
+//              percentVisible: slidePercent,
             ),
           ),
           PageIndicator(
@@ -46,10 +97,17 @@ class _HomePageState extends State<HomePage> {
               pages: viewModel,
               slideDirection: SlideDirection.rightToLeft,
               slidePercent: 0.3,
+//              activeIndex: activeIndex,
+//              pages: viewModel,
+//              slidePercent: slidePercent,
+//              slideDirection: slideDirection,
             ),
           ),
-//TODO(7.2)Bring in the Page adder to the top
-//          PageDragger(),
+          PageDragger(
+//            slideUpdateStream: streamController,
+//            canDragLeftToRight: activeIndex > 0,
+//            canDragRightToLeft: activeIndex < viewModel.length - 1,
+          ),
         ],
       ),
     );
--- lib/widget/page_dragger.dart
+++ lib/widget/page_dragger.dart
@@ -1,56 +1,113 @@
-//import 'package:flutter/material.dart';
-//import 'package:material_page_reveal/model/page_indicator_view_model.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:material_page_reveal/model/page_indicator_view_model.dart';
+
+class PageDragger extends StatefulWidget {
+  //TODO(8.2) Add a StreamController for SlideUpdate
+
+  //TODO(8.4)Lets solve the error due to drag beyond required
+
+//  final bool canDragLeftToRight;
+//  final bool canDragRightToLeft;
+//  final StreamController<SlideUpdate> slideUpdateStream;
 //
-//class PageDragger extends StatefulWidget {
-//  @override
-//  _PageDraggerState createState() => _PageDraggerState();
-//}
-//
-//class _PageDraggerState extends State<PageDragger> {
-//
-//static const FULL_DRAG = 300.0;
-//
-//  Offset dragStartPosition;
-//  SlideDirection slideDirection;
-//  double slidePercent;
-//
-//  onDragStart(DragStartDetails details){
-//    dragStartPosition = details.globalPosition;
-//    print('DragStartPositon:$dragStartPosition');
-//  }
-//  onDragUpdate(DragUpdateDetails details){
-//    if(dragStartPosition != null) {
-//      final newPosition = details.globalPosition;
-//      final dx = newPosition.dx - dragStartPosition.dx;
-//      if (dx < 0.0) {
+//  const PageDragger({
+//    Key key,
+//    this.slideUpdateStream,
+//    this.canDragLeftToRight,
+//    this.canDragRightToLeft,
+//  }) : super(key: key);
+
+  @override
+  _PageDraggerState createState() => _PageDraggerState();
+}
+
+class _PageDraggerState extends State<PageDragger> {
+  static const FULL_DRAG = 300.0;
+
+  Offset dragStartPosition;
+  SlideDirection slideDirection;
+  double slidePercent;
+
+  onDragStart(DragStartDetails details) {
+    dragStartPosition = details.globalPosition;
+    print('DragStartPositon:$dragStartPosition');
+  }
+
+  onDragUpdate(DragUpdateDetails details) {
+    if (dragStartPosition != null) {
+      final newPosition = details.globalPosition;
+      final dx = newPosition.dx - dragStartPosition.dx;
+//      if (dx < 0.0 && widget.canDragRightToLeft) {
 //        slideDirection = SlideDirection.rightToLeft;
-//      } else if (dx > 0.0) {
+//      } else if (dx > 0.0 && widget.canDragLeftToRight) {
 //        slideDirection = SlideDirection.leftToRight;
 //      } else {
 //        slideDirection = SlideDirection.none;
 //      }
-//
-//      slidePercent = (dx.abs()/FULL_DRAG).clamp(0.0, 1.0);
-//    }
-//
-//    print("Slide%=$slideDirection");
-//
-//  }
-//  onDragEnd(DragEndDetails details) {
-//    dragStartPosition = null;
-//    print('DragStartPositon:$dragStartPosition');
-//  }
-//
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return GestureDetector(
-//      onHorizontalDragStart: onDragStart,
-//      onHorizontalDragUpdate: onDragUpdate,
-//      onHorizontalDragEnd: onDragEnd,
+      if (dx < 0.0) {
+        slideDirection = SlideDirection.rightToLeft;
+      } else if (dx > 0.0) {
+        slideDirection = SlideDirection.leftToRight;
+      } else {
+        slideDirection = SlideDirection.none;
+      }
+
+      if(slideDirection != SlideDirection.none) {
+        slidePercent = (dx.abs() / FULL_DRAG).clamp(0.0, 1.0);
+      }else{
+        slidePercent = 0.0;
+      }
+    }
+
+//    widget.slideUpdateStream.add(
+//      SlideUpdate(
+//        direction: slideDirection,
+//        percent: slidePercent,
+//        dragUpdateType: DragUpdateType.dragging,
+//      ),
 //    );
-//  }
-//
-//
+    print("Slide%=$slideDirection");
+  }
+
+  onDragEnd(DragEndDetails details) {
+    dragStartPosition = null;
+//    widget.slideUpdateStream.add(
+//      SlideUpdate(
+//        direction: SlideDirection.none,
+//        percent: 0.0,
+//        dragUpdateType: DragUpdateType.doneDragging,
+//      ),
+//    );
+    print('DragStartPositon:$dragStartPosition');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onHorizontalDragStart: onDragStart,
+      onHorizontalDragUpdate: onDragUpdate,
+      onHorizontalDragEnd: onDragEnd,
+    );
+  }
+}
+
+//TODO:(8.3) Add enum for drag updates
+//enum DragUpdateType {
+//  dragging,
+//  doneDragging,
 //}
-//TODO:(7) Create a Dragger Widget
+//TODO:(8.1)Package the SlideDirection and slidePercent together to send a stream SlideUpdate
+
+//class SlideUpdate {
+//  SlideDirection direction;
+//  double percent;
+//  DragUpdateType dragUpdateType;
+//
+//  SlideUpdate({
+//    this.direction,
+//    this.percent,
+//    this.dragUpdateType,
+//  });
+//}
